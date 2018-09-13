1a6e81bfaa3ab4517e47769864b0a6bbf11ad4fa
R DeGroot <robert.b.degroot@gmail.com>
2018-09-13 08:41:23 -0400

    work started to configure home,vue
     - work started to configure home,vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,44 @@
 <template>
   <div class="home">
-    <h1>Home Page!</h1>
+    <h1>Holly's Landing Page Container</h1>
+    <div class='container'>
+      <ul>
+        <li><i class='fas fa-angle-right'></i>One</li>
+        <li><i class='fas fa-angle-right'></i>Two</li>
+        <li><i class='fas fa-angle-right'></i>Three</li>
+        <li><i class='fas fa-angle-right'></i>Four</li>
+        <li><i class='fas fa-angle-right'></i>Five</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Home'
-}
+  export default {
+    'name': 'Home'
+  }
 </script>
+
+<style scoped>
+  @import "https://cdn.jsdelivr.net/npm/animate.css@3.5.2/animate.min.css";
+  @import "https://use.fontawesome.com/releases/v5.3.1/css/all.css";
+
+  ul {
+    margin: 0;
+    padding: 0;
+    text-align: left;
+    list-style-type: none;
+  }
+
+  ul li {
+    padding-top: 25px;
+    font-size: 1.25em;
+    margin-bottom: 2px;
+    
+  }
+
+  i {
+    padding-right: 10px;
+    cursor: pointer;
+  }
+</style>
